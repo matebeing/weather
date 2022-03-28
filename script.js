8b6816c792caaafceb750ce9e7758e76fb5d84a7
Matheus Oliveira <51185184+matebeing@users.noreply.github.com>
2022-03-27 21:38:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', () => {
+    let lon;
+    let lat;
+
+    const city = document.getElementById('city');
+    const degree = document.getElementById('degree')
+    const summary = document.getElementById('summary')
+    const icon = document.getElementById('icon')
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            lon = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=pt_br&appid=9a4aa4da44c72132f455fafa96b4a94e`
+            fetch(api)
+                .then(response => response.json())
+                .then(data => {
+                    city.textContent = data.name;
+                    degree.textContent = `${data.main.temp} °C`;
+                    summary.textContent = data.weather[0].description;
+                    icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+                    console.log(`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+                })
+        })
+    }
+})
